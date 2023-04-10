d72e72ee626f193c5dcfd2ac1a5d847a65b41521
NIVESH PRITMANI <niveshpritmani@gmail.com>
2023-04-11 00:07:01 +0530

    splash activity code added

--- app/src/main/java/com/example/fitusionsports/SplashActivity.java
+++ app/src/main/java/com/example/fitusionsports/SplashActivity.java
@@ -6,6 +6,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -14,10 +19,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Animation splashanimation= AnimationUtils.loadAnimation(this,R.anim.splashanimation);
+        ImageView splashimage=findViewById(R.id.splash_image);
+        ProgressBar splashprogressbar=findViewById(R.id.splash_progress_bar);
+        splashimage.startAnimation(splashanimation);
         new Handler().postDelayed(() -> {
+            splashprogressbar.setVisibility(View.VISIBLE);
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-        },3000);
+        },10000);
     }
 }
