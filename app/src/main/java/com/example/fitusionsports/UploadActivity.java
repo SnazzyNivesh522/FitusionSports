29acef8c75ae67b1a2176c511ba036d3a8db8b2d
NIVESH PRITMANI <niveshpritmani@gmail.com>
2023-04-12 09:59:20 +0530

    fetching data and adding to firebase storage and realtime database

---
+++ app/src/main/java/com/example/fitusionsports/UploadActivity.java
@@ -0,0 +1,126 @@
+package com.example.fitusionsports;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class UploadActivity extends AppCompatActivity {
+    ImageView uploadimage;
+    Button savebutton;
+    EditText uploadtopic,uploaddesc,uploadlanguage;
+    String imageurl;
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+        uploadimage=findViewById(R.id.uploadimage);
+        uploadtopic=findViewById(R.id.uploadtopic);
+        uploaddesc=findViewById(R.id.uploaddesc);
+        uploadlanguage=findViewById(R.id.uploadlanguage);
+        savebutton=findViewById(R.id.savebutton);
+        ActivityResultLauncher<Intent> activityResultLauncher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode()== Activity.RESULT_OK){
+                            Intent data =result.getData();
+                            uri=data.getData();
+                            uploadimage.setImageURI(uri);
+                        }
+                        else{
+                            Toast.makeText(UploadActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        uploadimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photopicker=new Intent(Intent.ACTION_PICK);
+                photopicker.setType("image/*");
+                activityResultLauncher.launch(photopicker);
+            }
+        });
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savedata();
+                
+            }
+        });
+    }
+
+    public void savedata() {
+        StorageReference storageReference= FirebaseStorage.getInstance().getReference().child("Android Images").child(uri.getLastPathSegment());
+        AlertDialog.Builder builder=new AlertDialog.Builder(UploadActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog= builder.create();
+        dialog.show();
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                while(!uriTask.isComplete()){
+                    Uri urlImage=uriTask.getResult();
+                    imageurl=urlImage.toString();
+                    uploaddata();
+                    dialog.dismiss();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dialog.dismiss();
+            }
+        });
+    }
+    public void uploaddata(){
+        String title=uploadtopic.getText().toString();
+        String lang=uploadlanguage.getText().toString();
+        String desc=uploaddesc.getText().toString();
+        DataClass dataClass=new DataClass(title,desc,lang,imageurl);
+        String currentDate = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        FirebaseDatabase.getInstance().getReference("Android Tutorials").child(currentDate).setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(UploadActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
