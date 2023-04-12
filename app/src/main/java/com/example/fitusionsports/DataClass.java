89e0dbd312199b5fbbf5a36d92c75849ce4ad1a6
NIVESH PRITMANI <niveshpritmani@gmail.com>
2023-04-12 09:55:50 +0530

    getter and constructor and getter for data

---
+++ app/src/main/java/com/example/fitusionsports/DataClass.java
@@ -0,0 +1,31 @@
+package com.example.fitusionsports;
+
+public class DataClass {
+    private String dataTitle,dataDesc,dataLang,dataImage;
+
+    public String getDataTitle() {
+        return dataTitle;
+    }
+
+    public String getDataDesc() {
+        return dataDesc;
+    }
+
+    public String getDataLang() {
+        return dataLang;
+    }
+
+    public String getDataImage() {
+        return dataImage;
+    }
+
+    public DataClass() {
+    }
+
+    public DataClass(String dataTitle, String dataDesc, String dataLang, String dataImage) {
+        this.dataTitle = dataTitle;
+        this.dataDesc = dataDesc;
+        this.dataLang = dataLang;
+        this.dataImage = dataImage;
+    }
+}
