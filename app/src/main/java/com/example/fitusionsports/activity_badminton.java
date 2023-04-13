6866e1be8af92f7652cd26f307df678bf019d518
nij96969 <nijakbari@gmail.com>
2023-04-13 17:49:34 +0530

    added activity and layout for badminton

---
+++ app/src/main/java/com/example/fitusionsports/activity_badminton.java
@@ -0,0 +1,14 @@
+package com.example.fitusionsports;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_badminton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badminton);
+    }
+}
