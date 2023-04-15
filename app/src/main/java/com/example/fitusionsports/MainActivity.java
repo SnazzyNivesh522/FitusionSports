6f80267dc7e81eddc2cb00df47994535daa0d91b
NIVESH PRITMANI <niveshpritmani@gmail.com>
2023-04-15 20:52:06 +0530

    Main activity for navbar updated

--- app/src/main/java/com/example/fitusionsports/MainActivity.java
+++ app/src/main/java/com/example/fitusionsports/MainActivity.java
@@ -1,46 +1,53 @@
 package com.example.fitusionsports;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.fitusionsports.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
-    TextView login,register;
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        login=findViewById(R.id.login);
-        register=findViewById(R.id.register);
-        fab=findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,UploadActivity.class);
-                startActivity(intent);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomnavigation.setBackground(null);
+        binding.bottomnavigation.setOnItemSelectedListener(item -> {
+            switch ((int)item.getItemId()){
+                case (int)R.id.bottom_home:
+                    replacefragment(new HomeFragment());
+                    break;
+                case (int)R.id.bottom_fitness:
+                    replacefragment(new FitnessFragment());
+                    break;
+                case (int)R.id.bottom_forum:
+                    replacefragment(new ChatFragment());
+                    break;
+                case (int)R.id.bottom_person:
+                    replacefragment(new ProfileFragment());
+                    break;
             }
-        });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
+            return true;
         });
 
     }
+    private void replacefragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
