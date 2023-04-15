86e487ee96226a20ed5eb12db175d74c5647a3cd
nij96969 <nijakbari@gmail.com>
2023-04-15 23:07:58 +0530

    added OpenTrainingPageButton function

--- app/src/main/java/com/example/fitusionsports/HomePage.java
+++ app/src/main/java/com/example/fitusionsports/HomePage.java
@@ -1,7 +1,10 @@
 package com.example.fitusionsports;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+    public void OpenTrainingPageButton(View view){
+        Intent intent=new Intent(this,TrainingPage.class);
+        startActivity(intent);
     }
 }
