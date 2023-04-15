67c8aa3dcc3710b5da0207e19c9abdd91d94815d
nij96969 <nijakbari@gmail.com>
2023-04-15 23:06:26 +0530

    Solved Warnings and added onclick on badminton Image Button

--- app/src/main/java/com/example/fitusionsports/SportsLibraryActivity.java
+++ app/src/main/java/com/example/fitusionsports/SportsLibraryActivity.java
@@ -2,13 +2,23 @@ package com.example.fitusionsports;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class SportsLibraryActivity extends AppCompatActivity {
-
+    ImageButton BadmintonButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports_library);
+        BadmintonButton=findViewById(R.id.badmintonButton);
+    }
+    public void BadmintonPageOpen(View view){
+        Intent intent=new Intent(this,activity_badminton.class);
+        startActivity(intent);
     }
 }
