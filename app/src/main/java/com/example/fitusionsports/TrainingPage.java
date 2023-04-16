623ce508abe7b75350d25adf0dc313049aaa5eee
nij96969 <nijakbari@gmail.com>
2023-04-16 11:08:54 +0530

    added onclick listener to startWorkout

--- app/src/main/java/com/example/fitusionsports/TrainingPage.java
+++ app/src/main/java/com/example/fitusionsports/TrainingPage.java
@@ -2,7 +2,9 @@ package com.example.fitusionsports;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TrainingPage extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class TrainingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_page);
+    }
+    public void OpenTrainingPageStartWorkout(View view){
+        Intent StartWorkout=new Intent(this,TrainingPageWorkoutStart.class);
+        startActivity(StartWorkout);
     }
 }
